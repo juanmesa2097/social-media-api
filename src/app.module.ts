40dcb86caa0ee153f4ec2eafd0c22d1c03f03307
juanmesa2097 <juanmesa2097@gmail.com>
2021-10-02 17:34:17 -0500

    Import prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
 import { PostModule } from './modules/post/post.module';
 import { UserModule } from './modules/user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
 import { ConfigProviderModule } from './providers/config-provider.module';
 import { GqlProviderModule } from './providers/gql-provider.module';
 import { PubSubModule } from './pub-sub/pub-sub.module';
@@ -11,6 +12,7 @@ import { PubSubModule } from './pub-sub/pub-sub.module';
     ConfigProviderModule,
     GqlProviderModule,
     PubSubModule,
+    PrismaModule,
     AuthModule,
     UserModule,
     PostModule,
